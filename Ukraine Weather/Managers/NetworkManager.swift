5e137984ff270599701439a17b031e3a2b3bf900
Denis Vitios <denvitios@gmail.com>
2019-09-07 14:03:47 +0300

    progect  init, start working on arhitecture

---
+++ Ukraine Weather/Managers/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Ukraine Weather
+//
+//  Created by Denis on 9/7/19.
+//  Copyright © 2019 Denis. All rights reserved.
+//
+
+import Foundation
--- Ukraine Weather/ViewController.swift
+++ Ukraine Weather/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating {
+  
+
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    self.setupNavigationBar()
   }
 
-
+  fileprivate func setupNavigationBar() {
+    self.navigationItem.title = "Weather Application"
+    self.navigationController?.navigationBar.prefersLargeTitles = true
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    searchController.searchResultsUpdater = self
+    
+    navigationItem.searchController = searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+  }
+    //MARK: - UISearchResultsUpdating
+  func updateSearchResults(for searchController: UISearchController) {
+  print(searchController.searchBar.text ?? "")
+  }
+  //MARK: - UITableViewDataSource
+  
 }
 
